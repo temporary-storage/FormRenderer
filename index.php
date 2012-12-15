7522b852fc9ece11805b3365cd3bc2a8eb0e7d05
ghost_cfg <ghost_cfg@localhost.localdomain>
2012-12-15 02:03:25 +0200

    ; removed

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$a = 32;
+$b = 28;
+
+switch("+"){
+    default: $c=0;
+    case "+" : $c = $a+$b;
+    case "-" : $c = $a-$b;
+
+}
+//echo $c;
+//phpinfo();
+
+class MyClass{
+ public $lVar;
+ function __constructor(){
+     $this->lVar = 5;
+ }
+
+}
+var_dump($var);
+$var = new MyClass();
+//echo $var->lVar;
+
+interface First{function hi();}
+interface First1{function hi();}
+interface First2 extends First,First1{}
+abstract class mine implements First2{}
+
